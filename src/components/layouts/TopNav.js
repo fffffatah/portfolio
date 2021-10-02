ab113b40542faa4c8e0681b81fb3fd7368d4500e
A.F.M. Noorullah <ab.fatahmn@gmail.com>
2021-10-02 21:51:49 +0600

    hide experience

--- src/components/layouts/TopNav.js
+++ src/components/layouts/TopNav.js
@@ -38,9 +38,9 @@ export default function TopNav(){
                             {/* <NavItem>
                                 <Nav.Link eventKey="link-5" as={Link} to="/research">Research</Nav.Link>
                             </NavItem> */}
-                            <NavItem>
+                            {/* <NavItem>
                                 <Nav.Link eventKey="link-6" as={Link} to="/experience">Experience(s)</Nav.Link>
-                            </NavItem>
+                            </NavItem> */}
                         </Nav>
                     </Navbar.Collapse>
                     <Navbar.Collapse className="justify-content-end">
